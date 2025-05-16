c536aa8d41255120ab92f3b17d1b39455ea4f060
fulya <fnk1706@gmail.com>
2025-05-16 15:18:13 +0300

    8.gun

--- src/test/java/tests/day08_testbase_dropdown/C02_DropdownMenu.java
+++ src/test/java/tests/day08_testbase_dropdown/C02_DropdownMenu.java
@@ -1,7 +1,15 @@
 package tests.day08_testbase_dropdown;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import utilities.ReusableMethods;
 import utilities.TestBase_Each;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class C02_DropdownMenu extends TestBase_Each {
 
@@ -23,18 +31,68 @@ public class C02_DropdownMenu extends TestBase_Each {
 
         //1.Dogum tarihi gun seçeneğinden index kullanarak 5’i secin
 
+        //1. adım : dropdown webElementini locate edip kaydedelim
+        WebElement gunDropdown= driver.findElement(By.xpath("(//select[@class='form-control'])[1]"));
+        //2. adım: select objesi olustur parametre olrak dropdown elemnti yaz
+        Select selectDay = new Select(gunDropdown);
+        //3. adim: select objesi ile istenen islemi yapin
+        selectDay.selectByIndex(5);
+
 
         //2. Dogum tarihi ay seçeneğinden value kullanarak Nisan’i secin
 
+       // 1. adim:
+    WebElement ayDropdown = driver.findElement(By.xpath("(//select[@class='form-control'])[2]"));
+
+       //2. adim
+        Select selectAy = new Select(ayDropdown);
+        //3. adim
+        selectAy.selectByValue("nisan");
+
 
         //3. Dogum tarihi yil seçeneğinden visible text kullanarak 1990’i secin
+        // 1. adim:
+        WebElement yilDropdown = driver.findElement(By.xpath("(//select[@class='form-control'])[3]"));
+        //2. adim:
+        Select selectYil = new Select(yilDropdown);
+        //3. adim:
+        selectYil.selectByVisibleText("1990");
 
 
         //4. Secilen değerleri konsolda yazdirin
 
+        System.out.println(selectDay.getFirstSelectedOption().getText());
+        System.out.println(selectAy.getFirstSelectedOption().getText());
+        System.out.println(selectYil.getFirstSelectedOption().getText());
+
 
         //5. Ay dropdown menüdeki tum değerleri(value) yazdırın
+
+        List<WebElement> ayMenusuTumOptionElementleri = selectAy.getOptions();
+
+        // WebElementlerden olusan bir listeyi direkt yazdıramayız.
+        // for loop ile herbir elementi ele alıp
+        // webelement uzerindeki yazıyı olusturacagimiz bir string listeye ekleyelim
+
+        List<String> optionMetinleriList = new ArrayList<>();
+
+       for (WebElement eachElement: ayMenusuTumOptionElementleri){
+           optionMetinleriList.add(eachElement.getText());
+       }
+
+        System.out.println(optionMetinleriList);
+       // [Ay, Ocak, Şubat, Mart, Nisan, Mayıs, Haziran, Temmuz, Ağustos, Eylül, Ekim, Kasım, Aralık]
+
+
         //6. Ay Dropdown menusunun boyutunun 13 olduğunu test edin
+
+        int expectedMenuBoyutu = 13;
+        int actualMenuBoyutu = ayMenusuTumOptionElementleri.size();
+
+        Assertions.assertEquals(expectedMenuBoyutu,actualMenuBoyutu);
+
+
+        ReusableMethods.bekle(3);
 
     }
 
