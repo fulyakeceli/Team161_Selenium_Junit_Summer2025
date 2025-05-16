80f849aa4eca35dfa013fda4aafe8afe49cc2311
fulya <fnk1706@gmail.com>
2025-05-16 15:37:15 +0300

    8. gun

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,14 +1,48 @@
 package utilities;
 
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
+
 public class ReusableMethods {
 
-    public static void bekle (int saniye)  {
+    public static void bekle (int saniye) {
 
         try {
             Thread.sleep(saniye * 1000);
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+       // verilen WebElementşelerden olusan bir listedeki herbir webelementi ele alip
+       // webelement uzerindeki yazıyı olusturacagımız bir listeye ekleyip
+       // tum elementler elden gectikten sonra stringlerden olusan
+        // listeyi donduren bir method olusturun
+
+    public static List<String> stringListeDonustur (List<WebElement> webElementList){
+
+        List<String> stringList = new ArrayList<>();
+
+        for (WebElement eachElement:webElementList){
+            stringList.add(eachElement.getText());
+
+        }
+
+        return stringList;
+
+
+        }
+
+
+
+
+
+
+
     }
 
 
