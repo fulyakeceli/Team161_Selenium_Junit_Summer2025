258f3349b6e93f71108e91ebd1d1ee0db2cc9d49
fulya <fnk1706@gmail.com>
2025-05-22 12:04:14 +0300

    11.gun

--- src/test/java/tests/day12_actionsClass_FakerClass/C03_MoveToElement.java
+++ src/test/java/tests/day12_actionsClass_FakerClass/C03_MoveToElement.java
@@ -1,22 +1,65 @@
 package tests.day12_actionsClass_FakerClass;
 
-public class C03_MoveToElement {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.ReusableMethods;
+import utilities.TestBase_Each;
+
+public class C03_MoveToElement extends TestBase_Each {
+
+
+    @Test
+    public void test01() {
+
+        // 1- https://www.testotomasyonu.com/ adresine gidin
+        driver.get("https://www.testotomasyonu.com/");
+
+
+        //2- “Kids Wear” menusunun acilmasi icin mouse’u bu menunun ustune getirin
+
+        Actions actions = new Actions(driver);
+        ReusableMethods.bekle(2);
+
+        WebElement kidsWearMenu = driver.findElement(By.xpath("(//a[.='Kids Wear'])[3]"));
+        ReusableMethods.bekle(1);
+
+        actions.moveToElement(kidsWearMenu).perform();
+        ReusableMethods.bekle(1);
+
+
+        //3- “Boys” linkine basin
+        driver.findElement(By.linkText("Boys"))
+                .click();
+
+        ReusableMethods.bekle(2);
+
+        //4- Acilan sayfadaki ilk urunu tiklayin
+        driver.findElement(By.xpath("(//*[@class='prod-img'])[1]"))
+                .click();
+
+        ReusableMethods.bekle(2);
 
 
 
-    // 1- https://www.testotomasyonu.com/ adresine gidin
+        //4- Acilan sayfada urun isminin “Boys Shirt White Color” oldugunu test edin
+
+        WebElement urunIsimElementi = driver.findElement(By.xpath("//*[@class=' heading-sm mb-4']"));
+
+        ReusableMethods.bekle(1);
+
+        String expectedUrunIsmi = "Boys Shirt White Color";
+        String actualUrunIsmi = urunIsimElementi.getText();
+
+        Assertions.assertEquals(expectedUrunIsmi,actualUrunIsmi);
+
+        ReusableMethods.bekle(2);
 
 
 
-    //2- “Kids Wear” menusunun acilmasi icin mouse’u bu menunun ustune getirin
 
 
-
-    //3- “Boys” linkine basin
-
-
-    //4- Acilan sayfadaki ilk urunu tiklayin
-
-
-    //4- Acilan sayfada urun isminin “Boys Shirt White Color” oldugunu test edin
+    }
 }
